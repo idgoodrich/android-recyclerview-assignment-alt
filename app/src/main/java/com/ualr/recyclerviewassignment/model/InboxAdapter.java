57ca5f0898898fed134a3b6fda1c33cb6d6237c3
idgoodrich <35702432+idgoodrich@users.noreply.github.com>
2021-11-06 23:25:44 -0500

    maybe undrstand a bit??

---
+++ app/src/main/java/com/ualr/recyclerviewassignment/model/InboxAdapter.java
@@ -0,0 +1,61 @@
+package com.ualr.recyclerviewassignment.model;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+//TODO 11. Remove This https://guides.codepath.com/android/using-the-recyclerview thanks for the help
+public class InboxAdapter extends
+        RecyclerView.Adapter<InboxAdapter.ViewHolder> {
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View contactView = inflater.inflate(R.layout.item_contact, parent, false);
+
+        // Return a new holder instance
+        ViewHolder viewHolder = new ViewHolder(contactView);
+        return viewHolder;
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // Your holder should contain a member variable
+        // for any view that will be set as you render a row
+        public TextView nameTextView;
+        public TextView emailTextView;
+        public TextView msgTextView;
+
+        // We also create a constructor that accepts the entire item row
+        // and does the view lookups to find each subview
+        public ViewHolder(View itemView) {
+            // Stores the itemView in a public final member variable that can be used
+            // to access the context from any ViewHolder instance.
+            super(itemView);
+
+            nameTextView = (TextView) itemView.findViewById(R.id.contact_name);
+            emailTextView = (TextView) itemView.findViewById(R.id.emailAddr);
+            msgTextView = (TextView) itemView.findViewById(R.id.msgText);
+        }
+    }
+}
